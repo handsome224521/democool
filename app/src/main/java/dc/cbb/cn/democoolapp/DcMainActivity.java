859e861ef1b22333f749c0f2b6916dba4c48cc5b
handsome224521 <junhan911@126.com>
2017-10-01 12:56:06 +0800

    First commit

---
+++ app/src/main/java/dc/cbb/cn/democoolapp/DcMainActivity.java
@@ -0,0 +1,13 @@
+package dc.cbb.cn.democoolapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DcMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dc_main);
+    }
+}
